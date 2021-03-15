5ad41b253aab1aa81e38ff1cf04ba9be008969e7
sxsori <kkangmin19@gmail.com>
2021-03-15 13:26:23 +0900

    isec 0315

--- clone_app/lib/page/detail.dart
+++ clone_app/lib/page/detail.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider/carousel_slider.dart'; // 이미지 넘기는데 쓰인다
 
 class DetailContentView extends StatefulWidget {
   Map<String, String> data; // 데이터를 념겨받는 타입을 지정한다
@@ -21,7 +21,7 @@ class _DetailContentViewState extends State<DetailContentView> {
     _current = 0; //초기화
     imgList = [
       //이미지는 5장이야
-      {"id": "0 ", "url": widget.data["image"]},
+      {"id": "0 ", "url": widget.data["image"]}, // 아이디값에 따른 이미지
       {"id": "1 ", "url": widget.data["image"]},
       {"id": "2 ", "url": widget.data["image"]},
       {"id": "3 ", "url": widget.data["image"]},
@@ -36,7 +36,7 @@ class _DetailContentViewState extends State<DetailContentView> {
       leading: IconButton(
         //백스패이스 색부여 원본으로돌아가기
         onPressed: () {
-          Navigator.pop(context);
+          Navigator.pop(context); // stack 구조로 날려버리면된다
         },
         icon: Icon(Icons.arrow_back),
         color: Colors.white,
@@ -58,18 +58,20 @@ class _DetailContentViewState extends State<DetailContentView> {
 
   Widget _bodyWidget() {
     return Container(
-      child: Column(
+      child: Stack(
+        // 이미지와 이미지넘기는표시가 같이 있어야하기때문에
         children: [
           Hero(
+            // 이미지가 올라오면서 화면 보여주는 효과
             tag: widget.data["cid"], // 해당대크 아이디 위치해준다
             child: CarouselSlider(
               options: CarouselOptions(
                   // CarouselSlider 에 마우스를 올려보면 해당옵션이보임
-                  height: size.width,
+                  height: size.width, // 이미지폭 에 대비한다
                   initialPage: 0, //시작이미지
                   enableInfiniteScroll: false, // 스크롤을 무한이 한다
                   viewportFraction: 1, // 화면의 전체를 보여주게한다
-                  onPageChanged: (index, resaon) {
+                  onPageChanged: (index, reason) {
                     setState(() {
                       _current = index;
                     });
@@ -89,22 +91,26 @@ class _DetailContentViewState extends State<DetailContentView> {
               ).toList(),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: imgList.map((map) {
-              int index = imgList.indexOf(url);
-              return Container(
-                width: 8.0,
-                height: 8.0,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: _current == index
-                      ? Color.fromRGBO(0, 0, 0, 0.9)
-                      : Color.fromRGBO(0, 0, 0, 0.4),
-                ),
-              );
-            }).toList(),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.map((map) {
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == int.parse(map["id"])
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.3),
+                  ),
+                );
+              }).toList(),
+            ),
           ),
         ],
       ),
@@ -113,7 +119,7 @@ class _DetailContentViewState extends State<DetailContentView> {
 
   Widget _bottomBarWidget() {
     return Container(
-      width: size.width,
+      width: size.width, // 바텀 미디어 쿼리 사용으로 폭 설정
       height: 55,
       color: Colors.red,
     );
